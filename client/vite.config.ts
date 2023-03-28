acc6fd78b7de0c0717fa4be406a98b2041454a34
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-28 20:18:31 +0200

    try

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,56 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	resolve: {
+// 		preserveSymlinks: true,
+// 	},
+// 	// build: {
+// 	// 	rollupOptions: {
+// 	// 		external: "@mui/material/Box",
+// 	// 	},
+// 	// },
+// });
+
+// export default defineConfig({
+// 	base: "/Stakepool-Frontend/",
+// 	plugins: [vue()],
+// 	resolve: {
+// 		alias: {
+// 			"~": path.resolve(__dirname, "node_modules"),
+// 			"@": path.resolve(__dirname, "src"),
+// 		},
+// 	},
+// 	build: {
+// 		chunkSizeWarningLimit: 1600,
+// 	},
+// });
+
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		preserveSymlinks: true,
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: "globalThis",
+			},
+		},
 	},
-	// build: {
-	// 	rollupOptions: {
-	// 		external: "@mui/material/Box",
-	// 	},
-	// },
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id
+							.toString()
+							.split("node_modules/")[1]
+							.split("/")[0]
+							.toString();
+					}
+				},
+			},
+		},
+	},
 });
