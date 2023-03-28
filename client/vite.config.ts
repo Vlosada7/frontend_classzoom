238a0f7e02f23f66c5f781d5a547eabe1039e1d1
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-28 16:56:55 +0200

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    preserveSymlinks: true,
-  }
+	plugins: [react()],
+	resolve: {
+		preserveSymlinks: true,
+	},
+	build: {
+		rollupOptions: {
+			external: "@mui/material/Box",
+		},
+	},
 });
