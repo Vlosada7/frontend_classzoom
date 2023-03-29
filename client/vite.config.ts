212dc7278b062e3c317e2261c08c72ac674849ae
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-29 11:32:53 +0200

    aaaaaah

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,18 +6,18 @@ import solidPlugin from 'vite-plugin-solid';
 
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()],
-// 	resolve: {
-// 		preserveSymlinks: true,
-// 	},
-// 	build: {
-// 		chunkSizeWarningLimit: 1600,
-// 		rollupOptions: {
-// 			external: "@mui/material/Box",
-// 		},
-// 	},
-// });
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		preserveSymlinks: true,
+	},
+	build: {
+		chunkSizeWarningLimit: 10000,
+		rollupOptions: {
+			external: "@mui/material/Box",
+		},
+	},
+});
 
 // export default defineConfig({
 // 	base: "/Stakepool-Frontend/",
@@ -33,45 +33,46 @@ import solidPlugin from 'vite-plugin-solid';
 // 	},
 // });
 
-export default defineConfig({
-	plugins: [
-		react(),
-		solidPlugin(),
-    chunkSplitPlugin({
-      strategy: 'default',
-    })
-	],
-	optimizeDeps: {
-		esbuildOptions: {
-			// Node.js global to browser globalThis
-			define: {
-				global: "globalThis",
-			},
-		},
-	},
-	build: {
-		minify:'terser',
-		target: 'esnext',
-		terserOptions: {
-			output: {
-				comments: false, // This will remove all comments from the output files
-			},
-		},
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return id
-							.toString()
-							.split("node_modules/")[1]
-							.split("/")[0]
-							.toString();
-					}
-				},
+// export default defineConfig({
+// 	plugins: [
+// 		react(),
+// 		solidPlugin(),
+//     chunkSplitPlugin({
+//       strategy: 'default',
+//     })
+// 	],
+// 	optimizeDeps: {
+// 		esbuildOptions: {
+// 			// Node.js global to browser globalThis
+// 			define: {
+// 				global: "globalThis",
+// 			},
+// 		},
+// 	},
+// 	build: {
+// 		chunkSizeWarningLimit: 10000,
+// 		minify:'terser',
+// 		target: 'esnext',
+// 		terserOptions: {
+// 			output: {
+// 				comments: false, // This will remove all comments from the output files
+// 			},
+// 		},
+// 		rollupOptions: {
+// 			output: {
+// 				manualChunks(id) {
+// 					if (id.includes("node_modules")) {
+// 						return id
+// 							.toString()
+// 							.split("node_modules/")[1]
+// 							.split("/")[0]
+// 							.toString();
+// 					}
+// 				},
 				
-			},
-		},
-	},
-});
+// 			},
+// 		},
+// 	},
+// });
 
 
