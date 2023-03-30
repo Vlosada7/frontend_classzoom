6fcccecbbf662a97881d57b20c8301e3220b55c1
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-30 10:25:46 +0200

    add colyseus server

--- client/src/Phaser/scene/Game.tsx
+++ client/src/Phaser/scene/Game.tsx
@@ -40,7 +40,7 @@ export default class Game extends Phaser.Scene {
   private role = 'Student';
 
   // private client = new Client(import.meta.env.VITE_PHASER);
-  private client = new Client('ws://192.168.0.185:4001');
+  private client = new Client('wss://colyseus-classzoom.cyclic.app/');
   private room!: Room;
 
   private playerEntities: {
