2fdd932f6a12fc1937a614f3dec089a78933da68
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-30 13:49:40 +0200

    fixed avatars

--- client/src/utils/avatar.ts
+++ client/src/utils/avatar.ts
@@ -2,97 +2,120 @@ const avatar = {
   student: {
     Claire: {
       avatar: 'Claire',
-      image: '../../public/assets/Student/Claire/Claire-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Claire/Claire-single.png',
     },
     Damien: {
       avatar: 'Damien',
-      image: '../../public/assets/Student/Damien/Damien-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Damien/Damien-single.png',
     },
     Daniel: {
       avatar: 'Daniel',
-      image: '../../public/assets/Student/Daniel/Daniel-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Daniel/Daniel-single.png',
     },
     Dona: {
       avatar: 'Dona',
-      image: '../../public/assets/Student/Dona/Dona-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Dona/Dona-single.png',
     },
     Jake: {
       avatar: 'Jake',
-      image: '../../public/assets/Student/Jake/Jake-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Jake/Jake-single.png',
     },
     Janet: {
       avatar: 'Janet',
-      image: '../../public/assets/Student/Janet/Janet-single.png',
+      image:
+        '.https://udaan-classzoom.vercel.app/assets/Student/Janet/Janet-single.png',
     },
     Jennifer: {
       avatar: 'Jennifer',
-      image: '../../public/assets/Student/Jennifer/Jennifer-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Jennifer/Jennifer-single.png',
     },
     Joaquin: {
       avatar: 'Joaquin',
-      image: '../../public/assets/Student/Joaquin/Joaquin-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Joaquin/Joaquin-single.png',
     },
     Josh: {
       avatar: 'Josh',
-      image: '../../public/assets/Student/Josh/Josh-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Josh/Josh-single.png',
     },
     Kate: {
       avatar: 'Kate',
-      image: '../../public/assets/Student/Kate/Kate-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Kate/Kate-single.png',
     },
     Keiko: {
       avatar: 'Keiko',
-      image: '../../public/assets/Student/Keiko/Keiko-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Keiko/Keiko-single.png',
     },
     Kyle: {
       avatar: 'Kyle',
-      image: '../../public/assets/Student/Kyle/Kyle-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Kyle/Kyle-single.png',
     },
     Mathew: {
       avatar: 'Mathew',
-      image: '../../public/assets/Student/Mathew/Mathew-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Mathew/Mathew-single.png',
     },
     Mike: {
       avatar: 'Mike',
-      image: '../../public/assets/Student/Mike/Mike-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Mike/Mike-single.png',
     },
     Stacy: {
       avatar: 'Stacy',
-      image: '../../public/assets/Student/Stacy/Stacy-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Stacy/Stacy-single.png',
     },
     Valentin: {
       avatar: 'Valentin',
-      image: '../../public/assets/Student/Valentin/Valentin-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Valentin/Valentin-single.png',
     },
     Victor: {
       avatar: 'Victor',
-      image: '../../public/assets/Student/Victor/Victor-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Victor/Victor-single.png',
     },
     Yacine: {
       avatar: 'Yacine',
-      image: '../../public/assets/Student/Yacine/Yacine-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Yacine/Yacine-single.png',
     },
   },
   teacher: {
     Jordan: {
       avatar: 'Jordan',
-      image: '../../public/assets/Student/Jordan/Jordan-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Jordan/Jordan-single.png',
     },
     Karen: {
       avatar: 'Karen',
-      image: '../../public/assets/Student/Karen/Karen-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Karen/Karen-single.png',
     },
     Marie: {
       avatar: 'Marie',
-      image: '../../public/assets/Student/Marie/Marie-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Marie/Marie-single.png',
     },
     Mark: {
       avatar: 'Mark',
-      image: '../../public/assets/Student/Mark/Mark-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Mark/Mark-single.png',
     },
     Nancy: {
       avatar: 'Nancy',
-      image: '../../public/assets/Student/Nancy/Nancy-single.png',
+      image:
+        'https://udaan-classzoom.vercel.app/assets/Student/Nancy/Nancy-single.png',
     },
   },
 };
